bfe7d8e6fa490dd6c7215fd98d9abb327c97c62c
francesco-abritta <fra.abritta@gmail.com>
2022-02-02 17:28:11 +0100

    aggiunto input

---
+++ src/components/Input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <input type="text" v-model="inputSearch">
+      <button>Cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    
+}
+</script>
+
+<style>
+
+</style>
