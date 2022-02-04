1592731b0943c964d0d427dc32ee90ab7cefa0e5
francesco-abritta <fra.abritta@gmail.com>
2022-02-04 18:18:12 +0100

    aggiunte immagini poster

--- src/components/sections/Lista.vue
+++ src/components/sections/Lista.vue
@@ -7,12 +7,13 @@
                 <li>TITOLO ORIGINALE: {{elementi.original_title}}</li>
                 <li>
                     LINGUA: 
-                    <img v-if="elementi.original_language == 'it'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2560px-Flag_of_Italy.svg.png" alt="">
-                    <img v-else-if="elementi.original_language == 'en'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/640px-Flag_of_the_United_Kingdom.svg.png" alt="">
-                    <img v-else-if="elementi.original_language == 'es'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1024px-Flag_of_Spain.svg.png" alt="">
-                    <img v-else src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/1280px-International_Flag_of_Planet_Earth.svg.png" alt="">
+                    <img class="lingua" v-if="elementi.original_language == 'it'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2560px-Flag_of_Italy.svg.png" alt="">
+                    <img class="lingua" v-else-if="elementi.original_language == 'en'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/640px-Flag_of_the_United_Kingdom.svg.png" alt="">
+                    <img class="lingua" v-else-if="elementi.original_language == 'es'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1024px-Flag_of_Spain.svg.png" alt="">
+                    <img class="lingua" v-else src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/1280px-International_Flag_of_Planet_Earth.svg.png" alt="">
                 </li>
                 <li>VOTO: {{elementi.vote_average}}/10</li>
+                <li>POSTER: <img :src="linkImg + 'w342/' + elementi.poster_path" alt=""></li>
             </ul>
 
             <h1 v-if="series.length>0">Serie Tv</h1>
@@ -21,12 +22,13 @@
                 <li>TITOLO ORIGINALE: {{elements.original_name}}</li>
                 <li>
                     LINGUA: 
-                    <img v-if="elements.original_language == 'it'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2560px-Flag_of_Italy.svg.png" alt="">
-                    <img v-else-if="elements.original_language == 'en'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/640px-Flag_of_the_United_Kingdom.svg.png" alt="">
-                    <img v-else-if="elements.original_language == 'es'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1024px-Flag_of_Spain.svg.png" alt="">
-                    <img v-else src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/1280px-International_Flag_of_Planet_Earth.svg.png" alt="">
+                    <img class="lingua" v-if="elements.original_language == 'it'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2560px-Flag_of_Italy.svg.png" alt="">
+                    <img class="lingua" v-else-if="elements.original_language == 'en'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/640px-Flag_of_the_United_Kingdom.svg.png" alt="">
+                    <img class="lingua" v-else-if="elements.original_language == 'es'" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1024px-Flag_of_Spain.svg.png" alt="">
+                    <img class="lingua" v-else src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/1280px-International_Flag_of_Planet_Earth.svg.png" alt="">
                 </li>
                 <li>VOTO: {{elements.vote_average}}</li>
+                <li>POSTER: <img :src="linkImg + 'w342/' + elements.poster_path" alt=""></li>
             </ul>
 
       </div>
@@ -36,12 +38,17 @@
 <script>
 export default {
     name: 'Lista',
+    data(){
+        return{
+            linkImg: "https://image.tmdb.org/t/p/"
+        }
+    },
     props:{
         film :Array,
         series :Array
     },
 
-    //Metodo migliore assumendo di avere immagini salvate in locale 
+    //Metodo migliore per le img assumendo di avere immagini salvate in locale 
     //(in HTML <li><img :src="require(`../assets/${getFlag(element.lingua)}.png`)" alt=""></li>)
     //getFlag mi tira fuori la variabile urlFlag che sarà it en o altro e la aggiunge alla stringa di src
 
@@ -62,7 +69,7 @@ export default {
         margin-bottom: 20px;
     }
 
-    img{
+    .lingua{
         height: 10px;
         width: 15px;
     }
